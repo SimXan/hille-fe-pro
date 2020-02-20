a76adfc70b3ef48a610d0e073b91c13988a4f1be
SimXan <sahagangasuper@gmail.com>
2020-02-20 11:25:10 +0200

    first commit

---
+++ lesson-2/main.js
@@ -0,0 +1,11 @@
+alert (" Александр Ганжа ");
+
+alert (" 18 ");
+
+alert (" Hello World ");
+
+var x = " Александр Ганжа ";
+var y = " 18 " ;
+var z = " Hello World " ;
+var myAlert = x + y + z;
+alert(myAlert);
